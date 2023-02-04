ff139798250254249b0c3d5da48d6e3ca122b25c
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:29:17 +0100

    Update: AUTOSAR basic software service network management module for flexray communication protocol stack

--- main_R2023/infServiceFrNm_ServiceNvM_Types.hpp
+++ main_R2023/infServiceFrNm_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_ServiceFrNm_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_ServiceFrNm_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
